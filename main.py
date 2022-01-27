5bd315204e448d3aa2856d30eb41976419e928b4
Carlos Francisco Mendoza Lara <48531814+FranciscoMendozaLara@users.noreply.github.com>
2022-01-27 15:41:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ layout="wide")
 
 ###--- Read in data
 df = pd.read_csv('df_london.csv')
-# df_all = pd.read_csv('df_all_data_london.csv')
+df_all = pd.read_csv('df_all_data_london.csv')
 # Create column with hour
 df['hours'] = pd.DatetimeIndex(df['time']).hour
 
@@ -82,13 +82,13 @@ df_selection = df.query(
                     month == @month & \
                          day_of_week == @week"
 )
-# df_selection_all = df_all.query(
-#         "year == @year & \
-#             accident_severity == @severity &\
-#                 local_authority_district == @districts &\
-#                     month == @month & \
-#                          day_of_week == @week"
-# )
+df_selection_all = df_all.query(
+        "year == @year & \
+            accident_severity == @severity &\
+                local_authority_district == @districts &\
+                    month == @month & \
+                         day_of_week == @week"
+)
 
 ### Map
 fig = px.density_mapbox(df_selection, lat = 'latitude', lon = 'longitude',
@@ -109,48 +109,55 @@ st.plotly_chart(fig, use_container_width=True)
 
 ### Bar plots
 
-# bars per hour
+# cars per hour
+st.header('Number of accidents per hour')
 hour_accident_df = df_selection['hours'].value_counts()
 fig = px.bar(hour_accident_df)
 st.plotly_chart(fig, use_container_width=True)
 
 #histogram
+st.header('Accident severity by district')
 df_bars = df_selection.groupby(['local_authority_district','year'])['accident_severity_inv'].sum().reset_index()
 fig = px.histogram(df_bars, x='local_authority_district', y='accident_severity_inv', color='year', barmode='group',
 labels={ "accident_severity_inv": "Accident severity",
 "local_authority_district": "District"})
 st.plotly_chart(fig, use_container_width=True)
 
+st.header('Number of casualties by district')
 df_bars = df_selection.groupby(['local_authority_district','year'])['number_of_casualties'].sum().reset_index()
 fig = px.histogram(df_bars, x='local_authority_district', y='number_of_casualties', color='year', barmode='group',
 labels={ "number_of_casualties": "Number of casualties" ,
 "local_authority_district": "District"})
 st.plotly_chart(fig, use_container_width=True)
 
+st.header('Severity by district')
 df_bars = df_selection.groupby(['local_authority_district','year'])['severity'].sum().reset_index()
 fig = px.histogram(df_bars, x='local_authority_district', y='severity', color='year', barmode='group',
 labels={ "severity": "Severity",
 "local_authority_district": "District" })
 st.plotly_chart(fig, use_container_width=True)
 
+# Histogram
+st.header('Distribution of cumber of casualties')
 x1 = df_selection[['number_of_casualties', 'year']]
 fig = px.histogram(x1, x="number_of_casualties", color = 'year')
 st.plotly_chart(fig, use_container_width=True)
 
-fig = px.pie(df_selection, values='number_of_vehicles', names='accident_severity', title='Number of vehicles by accident severity')
+# Pie charts
+fig = px.pie(df_selection_all, values='number_of_vehicles', names='accident_severity', title='Number of vehicles by accident severity')
 st.plotly_chart(fig, use_container_width=True)
 
-fig = px.pie(df_selection, values='number_of_vehicles', names='sex_of_driver', title = 'Number of vehicles by sex of driver')
+fig = px.pie(df_selection_all, values='number_of_vehicles', names='sex_of_driver', title = 'Number of vehicles by sex of driver')
 st.plotly_chart(fig, use_container_width=True)
 
-fig = px.pie(df_selection, values='number_of_vehicles', names='age_band_of_driver', title = 'Number of vehicles by age of driver')
+fig = px.pie(df_selection_all, values='number_of_vehicles', names='age_band_of_driver', title = 'Number of vehicles by age of driver')
 st.plotly_chart(fig, use_container_width=True)
 
-100*df_selection.pivot_table('severity', index='day_of_week', 
-columns='year', aggfunc='sum')/df_selection.pivot_table('severity', index='day_of_week', columns='year', aggfunc='sum').sum()
+100*df_selection_all.pivot_table('severity', index='day_of_week', 
+columns='year', aggfunc='sum')/df_selecdf_selection_alltion.pivot_table('severity', index='day_of_week', columns='year', aggfunc='sum').sum()
 
-100*df_selection.pivot_table('severity', index='month', columns='year',
- aggfunc='sum')/df_selection.pivot_table('severity', index='month', columns='year', aggfunc='sum').sum()
+100*df_selection_all.pivot_table('severity', index='month', columns='year',
+ aggfunc='sum')/df_selection_all.pivot_table('severity', index='month', columns='year', aggfunc='sum').sum()
 
 
 st.image("images\cars1.jpg")
